e964c2f3209fe7f9d7bed4e13536742d1b4d36b6
Soomin Lee <whonineofive@gmail.com>
2023-12-17 16:13:33 -0500

    first commit

---
+++ Assets/Scripts/MoveJump.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class MoveJump : MonoBehaviour
+{
+    float horizontal;
+    public float speed = 10;
+    public float jumpingPower = 10;
+    private bool isFacingRight = true;
+    public Rigidbody2D rigid;
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        /*        if (Input.GetButtonDown("Jump"))
+                {
+                    rigid.velocity = new Vector2(rigid.velocity.x, jumpingPower);
+                }*/
+
+        Debug.Log(rigid.velocity.y);
+        if (Input.GetButtonDown("Jump") && (rigid.velocity.y <= 0f && rigid.velocity.y > -0.1f))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, jumpingPower);
+            
+        }
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(horizontal * speed, rigid.velocity.y);
+    }
+
+
+    private void Flip()
+    {
+        if (horizontal > 0f)
+        {
+            spriteRenderer.flipX = false;   
+        }
+        else if (horizontal < 0f)
+        {
+            spriteRenderer.flipX = true;
+        }
+
+
+/*        if (isFacingRight)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }*/
+    }
+}
